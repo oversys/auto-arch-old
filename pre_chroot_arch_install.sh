05e5f05f9c1ae2b3353b7b72d196a3f5ac439cc5
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-06 17:57:36 +0300

    Update pre_chroot_arch_install.sh

--- pre_chroot_arch_install.sh
+++ pre_chroot_arch_install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Get Boot Partition
-echo "Boot Partition (/dev/sdaX):"
-read BOOT_PART
-if [[ $BOOT_PART  =~ "/dev/sda" ]]
+echo "Boot Partition (sdaX):"
+read BOOT_SPART
+if [[ $BOOT_SPART  =~ "sda" ]]
 then
     echo "Valid boot partition."
-    echo $BOOT_PART > boot_part.txt
+    echo $BOOT_SPART > boot_spart.txt
 else
     echo "Invalid input."
     exit
 fi
 
 # Get Root Partition
-echo "Root Partition (/dev/sdaX):"
-read ROOT_PART
-if [[ $ROOT_PART =~ "/dev/sda" ]]
+echo "Root Partition (sdaX):"
+read ROOT_SPART
+if [[ $ROOT_SPART =~ "sda" ]]
 then
     echo "Valid root partition."
 else
@@ -24,9 +24,11 @@ else
 fi
 
 # Format Partitions
-mkfs.fat -F32 $BOOT_PART
-mkfs.ext4 $ROOT_PART
-mount $ROOT_PART /mnt
+BOOT_PART=/dev/"$BOOT_SPART"
+ROOT_PART=/dev/"$ROOT_SPART"
+mkfs.fat -F32 "$BOOT_PART"
+mkfs.ext4 "$ROOT_PART"
+mount "$ROOT_PART" /mnt
 
 # Install System
 pacstrap /mnt base linux linux-firmware
