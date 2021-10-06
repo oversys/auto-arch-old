349ccb544798faee95e8b051aeac6f421ab48c3f
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-06 12:02:10 +0300

    Add files via upload

---
+++ chroot_arch_install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Set the time zone
+echo "Region:"
+read REGION
+echo "City:"
+read CITY
+
+ln -sf /usr/share/zoneinfo/$REGION/$CITY /etc/localtime
+hwclock --systohc
+
+# Localization
+sed -i "s/#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
+locale-gen
+
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# Hosts
+echo "Enter hostname:"
+read HOSTNAME
+
+echo $HOSTNAME > /etc/hostname
+echo "127.0.0.1  localhost" >> /etc/hosts
+echo "::1        localhost" >> /etc/hosts
+echo "127.0.1.1  $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
+
+# Boot Settings
+pacman -Syu
+pacman -S --noconfirm grub efibootmgr mtools os-prober dosfstools
+mkdir /boot/EFI
+
+# Read Selected Boot Partition
+while IFS= read -r line
+do
+    BOOT_PART=$line
+done < boot_part.txt
+
+# Install GRUB
+mount $BOOT_PART /boot/EFI
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Adding a user
+echo "Enter username:"
+read USERNAME
+useradd -m $USERNAME
+usermod -aG wheel,audio,video $USERNAME
+
+# Setting Passwords
+echo "Set password for ROOT:"
+passwd
+
+echo "Set password for $USERNAME:"
+passwd $USERNAME
+
+# Configure Sudo
+pacman -S sudo
+echo "$USERNAME  ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+# Done
+echo "Basic installation complete. Install additional packages, desktop environments and/or window managers and configure network."
---
+++ pre_chroot_arch_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Get Boot Partition
+echo "Boot Partition (/dev/sdaX):"
+read BOOT_PART
+if [[ $BOOT_PART  =~ "/dev/sda" ]]
+then
+    echo "Valid boot partition."
+    echo $BOOT_PART > boot_part.txt
+else
+    echo "Invalid input."
+    exit
+fi
+
+# Get Root Partition
+echo "Root Partition (/dev/sdaX):"
+read ROOT_PART
+if [[ $ROOT_PART =~ "/dev/sda" ]]
+then
+    echo "Valid root partition."
+else
+    echo "Invalid input."
+    exit
+fi
+
+# Format Partitions
+mkfs.fat -F32 $BOOT_PART
+mkfs.ext4 $ROOT_PART
+mount $ROOT_PART /mnt
+
+# Install System
+pacstrap /mnt base linux linux-firmware
+
+# Generate fstab file
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Part One Done
+echo "Pre-Chroot installation complete."
