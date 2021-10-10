2dfe44e0d3389aae28a7f96eeaf5c594ee194107
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-10 11:56:43 +0300

    Update awesomewm2.sh

--- awesomewm2.sh
+++ awesomewm2.sh
@@ -3,7 +3,7 @@
 # Get awesomewm configuration
 mkdir .config
 mkdir /home/$USER/.config/awesome
-curl -L tinyurl.com/vjh-awesomewm-config > /home/$USER/.config/awesome/rc.lua
+sudo curl -L tinyurl.com/vjh-awesomewm-config > /home/$USER/.config/awesome/rc.lua
 
 # Get wallpaper
 wget -O /home/$USER/wallpaper.jpg https://github.com/BetaLost/Arch-Install-Script/blob/main/wallpaper.jpg?raw=true
@@ -15,7 +15,7 @@ makepkg -si
 cd ../
 rm -rf polybar
 mkdir /home/$USER/.config/polybar
-curl -L tinyurl.com/vjh-polybar-config > /home/$USERNAME/.config/polybar/config
+sudo curl -L tinyurl.com/vjh-polybar-config > /home/$USERNAME/.config/polybar/config
 
 # Get picom
 git clone https://aur.archlinux.org/picom-ibhagwan-git.git
@@ -24,7 +24,7 @@ makepkg -si
 cd ../
 rm -rf picom-ibhagwan-git
 mkdir /home/$USERNAME/.config/picom
-cp /etc/xdg/picom.conf.example /home/$USERNAME/.config/picom/picom.conf
+sudo cp /etc/xdg/picom.conf.example /home/$USERNAME/.config/picom/picom.conf
 
 printf "#!/bin/bash\nkillall -q polybar\nwhile pgrep -u $UID -x polybar >/dev/null; do sleep 1; done\npolybar default &" >> /home/$USERNAME/.config/polybar/launch.sh
-chmod +x /home/$USERNAME/.config/polybar/launch.sh
+sudo chmod +x /home/$USERNAME/.config/polybar/launch.sh
