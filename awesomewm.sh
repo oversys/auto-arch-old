2c6421e11a6d7416073a9fc1c0f44bdf8dc73873
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-11 09:10:30 +0300

    Update awesomewm.sh

--- awesomewm.sh
+++ awesomewm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install packages
-pacman -Syu --noconfirm lightdm lightdm-gtk-greeter awesome xorg-server xterm wget git ttf-dejavu ttf-fira-sans network-manager-applet feh
-echo -e "\e[92m\e[1mInstalled window manager packages.\e[m"
+pacman -Syu --noconfirm lightdm lightdm-gtk-greeter awesome xorg-server alacritty wget git ttf-dejavu ttf-fira-sans ttf-fira-mono network-manager-applet feh powerline-fonts
+echo -e "\e[92m\e[1mInstalled packages.\e[m"
 
 # Get username
 while IFS= read -r line
@@ -17,5 +17,15 @@ groupadd -r autologin
 gpasswd -a $USERNAME autologin
 systemctl enable lightdm.service
 echo -e "\e[92m\e[1mEnabled display manager.\e[m"
+
+# Better terminal
+git clone --recursive https://github.com/andresgongora/synth-shell.git
+chmod +x synth-shell/setup.sh
+synth-shell/setup.sh
+curl -L tinyurl.com/vjh-synth-shell > /home/$USERNAME/.config/synth-shell/synth-shell-prompt.config
+
+# Get alacritty configuration
+mkdir /home/$USERNAME/.config/alacritty
+curl -L tinyurl.com/vjh-alacritty > /home/$USERNAME/.config/alacritty/alacritty.yml
 
 rm username.txt $0
