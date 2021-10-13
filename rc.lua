a23be3cabfb68af2f95eec50b927a9c46c27b59b
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-13 08:16:39 +0300

    Update rc.lua

--- rc.lua
+++ rc.lua
@@ -53,7 +53,7 @@ editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Set wallpaper
--- awful.spawn.with_shell("feh --bg-scale /home/chief/wallpaper.png")
+awful.spawn.with_shell("feh --bg-scale /home/chief/wallpaper.png")
 awful.spawn.with_shell("wal -i /home/chief/wallpaper.png")
 
 -- Default modkey.
