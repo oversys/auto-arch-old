15efeffd944c2a502ca2874296f9c5daba8b399c
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-13 11:47:55 +0300

    Update awesomewm2.sh

--- awesomewm2.sh
+++ awesomewm2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Get awesomewm configuration
-mkdir /home/$USER/.config/
-mkdir /home/$USER/.config/awesome
-sudo curl -L tinyurl.com/vjh-awesomewm-config > /home/$USER/.config/awesome/rc.lua
+mkdir $HOME/.config/
+mkdir $HOME/.config/awesome
+sudo curl -L tinyurl.com/vjh-awesomewm-config > $HOME/.config/awesome/rc.lua
 
 # Get wallpaper
-wget -O /home/$USER/wallpaper.png https://github.com/BetaLost/Arch-Install-Script/blob/main/wallpaper.png?raw=true
+wget -O $HOME/wallpaper.png https://github.com/BetaLost/Arch-Install-Script/blob/main/wallpaper.png?raw=true
 
 # Terminal colorscheme
-printf "\n(cat ~/.cache/wal/sequences &)" >> /home/$USER/.bashrc
+printf "\n(cat ~/.cache/wal/sequences &)" >> $HOME/.bashrc
 
 # Get brave
 git clone https://aur.archlinux.org/brave-bin.git
@@ -22,13 +22,13 @@ cd polybar
 makepkg -si --noconfirm
 cd ../
 rm -rf polybar
-mkdir /home/$USER/.config/polybar
-sudo curl -L tinyurl.com/vjh-polybar-config > /home/$USER/.config/polybar/config
+mkdir $HOME/.config/polybar
+sudo curl -L tinyurl.com/vjh-polybar-config > $HOME/.config/polybar/config
 
 # Get polywins
 git clone https://github.com/tam-carre/polywins.git
-mkdir /home/$USER/.config/polybar/scripts
-PW_FILE="/home/$USER/.config/polybar/scripts/polywins.sh"
+mkdir $HOME/.config/polybar/scripts
+PW_FILE="$HOME/.config/polybar/scripts/polywins.sh"
 mv polywins/polywins.sh $PW_FILE
 rm -rf polywins
 sed -i "s/active_text_color=\"#250F0B\"/active_text_color=\"#FFFFFF\"/g" $PW_FILE
@@ -41,12 +41,12 @@ cd picom-ibhagwan-git
 makepkg -si --noconfirm
 cd ../
 rm -rf picom-ibhagwan-git
-mkdir /home/$USER/.config/picom
-sudo cp /etc/xdg/picom.conf.example /home/$USER/.config/picom/picom.conf
+mkdir $HOME/.config/picom
+sudo cp /etc/xdg/picom.conf.example $HOME/.config/picom/picom.conf
 
 # Get alacritty configuration
-mkdir /home/$USER/.config/alacritty
-curl -L tinyurl.com/vjh-alacritty > /home/$USER/.config/alacritty/alacritty.yml
+mkdir $HOME/.config/alacritty
+curl -L tinyurl.com/vjh-alacritty > $HOME/.config/alacritty/alacritty.yml
 
-printf '#!/bin/bash\nkillall -q polybar\nwhile pgrep -u '"$UID -x polybar >/dev/null; do sleep 1; done\npolybar default &" >> /home/$USER/.config/polybar/launch.sh
-sudo chmod +x /home/$USER/.config/polybar/launch.sh
+printf '#!/bin/bash\nkillall -q polybar\nwhile pgrep -u '"$UID -x polybar >/dev/null; do sleep 1; done\npolybar default &" >> $HOME/.config/polybar/launch.sh
+sudo chmod +x $HOME/.config/polybar/launch.sh
