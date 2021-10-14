9ac325d6bb5f7ae015b7e827ece987498e1cac88
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-14 09:13:18 +0300

    Update awesomewm.sh

--- awesomewm.sh
+++ awesomewm.sh
@@ -6,7 +6,7 @@
 pacman -Syu --noconfirm
 
 # Display manager
-pacman -S --noconfirm lightdm lightdm-gtk-greeter
+pacman -S --noconfirm lightdm lightdm-webkit2-greeter
 
 # Window Manager
 pacman -S --noconfirm awesome xorg-server ttf-dejavu
@@ -32,10 +32,14 @@ do
 done < username.txt
 
 # Lightdm autologin
-echo "autologin-user=$USERNAME" >> /etc/lightdm/lightdm.conf
-echo "autologin-session=awesome" >> /etc/lightdm/lightdm.conf
-groupadd -r autologin
-gpasswd -a $USERNAME autologin
+git clone https://github.com/NoiSek/Aether.git
+cp Aether /usr/share/lightdm-webkit/themes/
+sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = lightdm-webkit-theme-aether #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+ln -s /usr/share/lightdm-webkit/themes/Aether /usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether
+sed -i "s/# greeter-session = Session to load for greeter/greeter-session = lightdm-webkit2-greeter/g" /etc/lightdm/lightdm.conf
+sed -i "s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/g" /etc/lightdm/lightdm.conf
+sed -i "s/# user-session = Session to load for users/user-session = awesome/g" /etc/lightdm/lightdm.conf
+sed -i "s/#user-session=default/user-session=awesome/g" /etc/lightdm/lightdm.conf
 systemctl enable lightdm.service
 echo -e "\e[92m\e[1mEnabled display manager.\e[m"
 
