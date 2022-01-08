e6990387736db16d89d381389bf3405106c81991
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2022-01-08 19:39:48 +0400

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -158,6 +158,7 @@ echo -e "\e[32m\e[1mConfigured the BASH shell.\e[m"
 # Configure VIM
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mv $HOME/dotfiles/.vimrc $HOME/
+vim +PlugInstall +qa
 
 echo -e "\e[32m\e[1mConfigured VIM.\e[m"
 
