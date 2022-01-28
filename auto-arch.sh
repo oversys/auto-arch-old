6617b2e444b741831848e7c6cd87a11cedfbed96
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2022-01-28 14:59:30 +0400

    Update auto-arch.sh

--- auto-arch.sh
+++ auto-arch.sh
@@ -11,11 +11,11 @@ echo -e "\e[32m\e[1mChanged Font.\e[m"
 curl -Lo pre-chroot.sh https://raw.githubusercontent.com/BetaLost/auto-arch/main/pre-chroot.sh
 bash pre-chroot.sh
 
-arch-chroot /mnt "curl -Lo chroot.sh https://raw.githubusercontent.com/BetaLost/auto-arch/main/chroot.sh; bash chroot.sh"
+arch-chroot /mnt /bin/bash -c "curl -Lo chroot.sh https://raw.githubusercontent.com/BetaLost/auto-arch/main/chroot.sh; bash chroot.sh"
 
 USERNAME=$(cat /mnt/username.txt)
 rm /mnt/username.txt
 echo "Running script as \"$USERNAME\".."
-arch-chroot /mnt "curl -Lo post-install.sh https://raw.githubusercontent.com/BetaLost/auto-arch/main/post-install.sh; su -c \"cd; zsh post-install.sh\" $USERNAME -"
+arch-chroot /mnt /bin/bash -c "cd /home/$USERNAME; curl -Lo post-install.sh https://raw.githubusercontent.com/BetaLost/auto-arch/main/post-install.sh; su -c \"zsh post-install.sh\" $USERNAME -"
 
 echo -e "\e[32m\e[1mAuto-arch script completed!\e[m"
