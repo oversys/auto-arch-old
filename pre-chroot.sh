591e6aa425edd4527f116aff984dccb1aeeb55dd
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2022-02-08 12:15:20 +0400

    Update pre-chroot.sh

--- pre-chroot.sh
+++ pre-chroot.sh
@@ -3,11 +3,15 @@
 # Parse Variables
 BOOTDEV=$1
 ROOTDEV=$2
+CHOICE=$3
 
-# Infobox Function
+# Functions
 infobox() {
-	whiptail --backtitle "Auto Arch" --title "$1" --infobox "$2" 8 0
+	whiptail --backtitle "Auto Arch" --title "$1" --infobox "$2" 12 0
 }
+
+getdesc() { pacman -Si $1 | grep -Po '^Description\s*: \K.+'; }
+getsize() { pacman -Si $1 | grep -Po '^Installed Size\s*: \K.+'; }
 
 # Update the System Clock
 infobox "System Clock" "Updating system clock..."
@@ -29,8 +33,39 @@ infobox "Parallel Downloading" "Enabling parallel downloading in pacman..."
 sed -i "s/#ParallelDownloads/ParallelDownloads/g" /etc/pacman.conf
 
 # Install System
-infobox "Base System" "Installing base system..."
-pacstrap /mnt base linux linux-firmware linux-headers base-devel dkms intel-ucode libnewt > /dev/null
+PKGS=(
+	"base"
+	"linux"
+	"linux-firmware"
+	"linux-headers"
+	"base-devel"
+	"dkms"
+	"intel-ucode"
+	"libnewt"
+)
+
+getindex() {
+	for i in "${!PKGS[@]}"; do
+		if [[ "${PKGS[i]}" = "$1" ]]; then echo $(expr $i + 1); fi
+	done
+}
+
+fastinstall() {
+	infobox "Base System" "Installing base system..."
+	pacstrap /mnt "${PKGS[@]}" > /dev/null
+}
+
+slowinstall() {
+	for pkg in "${PKGS[@]}"; do
+		infobox "Base System Installation" "Name: $pkg\nDescription: $(getdesc $pkg)\nSize: $(getsize $pkg)\n$(getindex $pkg) out of ${#PKGS[@]}"
+		pacstrap /mnt $pkg > /dev/null
+	done
+}
+
+case $CHOICE in
+	1) fastinstall;;
+	2) slowinstall;;
+esac
 
 # Generate fstab file
 infobox "fstab" "Generating fstab file..."
